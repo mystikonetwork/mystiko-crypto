6fb8146c03b9536e80df20ab602219ddab216eb8
Fitch Li <fitch@mystiko.network>
2022-09-07 12:12:09 +0800

    remove unused vars

--- packages/secret-share/tests/shamir.test.ts
+++ packages/secret-share/tests/shamir.test.ts
@@ -1,8 +1,7 @@
-import { FIELD_SIZE } from '@mystikonetwork/utils';
 import { SecretSharing } from '../src';
 
 test('test secret sharing', () => {
-  const secret = SecretSharing.random(FIELD_SIZE);
+  const secret = SecretSharing.random();
   const { shares, threshold } = SecretSharing.split(secret, 30, 17);
   const recoveredSecret = SecretSharing.recover(shares.sort(() => Math.random() - 0.5).slice(0, threshold));
   expect(recoveredSecret.toString()).toBe(secret.toString());
