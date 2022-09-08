5be4c193a637d60c1d030d396ac4ef9efe76a706
Fitch Li <fitch@mystiko.network>
2022-09-08 22:55:17 +0800

    fix some issues related circuits

--- packages/ecies/src/ecies.ts
+++ packages/ecies/src/ecies.ts
@@ -1,4 +1,4 @@
-import { FIELD_SIZE, toBN, toBuff, toFixedLenHexNoPrefix, toHexNoPrefix } from '@mystikonetwork/utils';
+import { FIELD_SIZE, toBN, toHexNoPrefix } from '@mystikonetwork/utils';
 import BN from 'bn.js';
 import { babyjub, poseidon } from 'circomlibjs';
 import randomBytes from 'randombytes';
@@ -10,32 +10,42 @@ export class ECIES {
     return toBN(toHexNoPrefix(randomBytes(KEY_LEN)), 16).mod(FIELD_SIZE);
   }
 
-  public static publicKey(secretKey: BN): BN {
+  public static publicKey(secretKey: BN): Buffer {
     const pk = babyjub.mulPointEscalar(babyjub.Base8, secretKey.toString());
-    return toBN(toHexNoPrefix(babyjub.packPoint(pk)), 16);
+    return babyjub.packPoint(pk);
   }
 
-  public static unpackPublicKey(publicKey: BN): { x: BN; y: BN } {
-    const pkBuffer = toBuff(toFixedLenHexNoPrefix(publicKey, KEY_LEN));
-    const unpacked = babyjub.unpackPoint(pkBuffer);
+  public static unpackPublicKey(publicKey: Buffer): { x: BN; y: BN } {
+    const unpacked = babyjub.unpackPoint(publicKey);
     return { x: toBN(unpacked[0].toString()), y: toBN(unpacked[1].toString()) };
   }
 
-  public static encrypt(plain: BN, pk: BN, commonSk: BN): BN {
+  public static encrypt(plain: BN, pk: Buffer, commonSk: BN): BN {
     const unpackedPk = ECIES.unpackPublicKey(pk);
     const k = babyjub.mulPointEscalar(
       [babyjub.F.e(unpackedPk.x.toString()), babyjub.F.e(unpackedPk.y.toString())],
       commonSk.toString(),
     );
-    return plain.add(toBN(poseidon(k).toString()));
+    return ECIES.mod(plain.add(toBN(poseidon(k).toString())));
   }
 
-  public static decrypt(encrypted: BN, sk: BN, commonPk: BN): BN {
+  public static decrypt(encrypted: BN, sk: BN, commonPk: Buffer): BN {
     const unpackedGeneratedPk = ECIES.unpackPublicKey(commonPk);
     const k = babyjub.mulPointEscalar(
       [babyjub.F.e(unpackedGeneratedPk.x.toString()), babyjub.F.e(unpackedGeneratedPk.y.toString())],
       sk.toString(),
     );
-    return encrypted.sub(toBN(poseidon(k)));
+    return ECIES.mod(encrypted.sub(toBN(poseidon(k))));
+  }
+
+  private static mod(aNumber: BN): BN {
+    if (aNumber.gten(0)) {
+      return aNumber.mod(FIELD_SIZE);
+    }
+    let remain = aNumber.mod(FIELD_SIZE).abs();
+    if (!remain.isZero()) {
+      remain = FIELD_SIZE.sub(remain);
+    }
+    return remain;
   }
 }
--- packages/ecies/tests/ecies.test.ts
+++ packages/ecies/tests/ecies.test.ts
@@ -1,11 +1,11 @@
-import { toBN } from '@mystikonetwork/utils';
+import { FIELD_SIZE, toBN, toHex } from '@mystikonetwork/utils';
 import { ECIES } from '../src';
 
 test('test secret key/public key', () => {
   const sk = toBN('6653394197986049121374360682484777687085153755365159319914007593958096971366');
   const pk = ECIES.publicKey(sk);
   const unpackedPk = ECIES.unpackPublicKey(pk);
-  expect(pk.toString()).toBe('54076255637382257347592018492939936661737837603026694521812985613489874914839');
+  expect(toHex(pk)).toBe('0x778e137829c9d7e2c60c6b650ea759c9a823c8a88b7b0734c48f2a3108a3ce17');
   expect(unpackedPk.x.toString()).toBe(
     '6475168065192867422305723429513217789134682778204070412258950526142122128968',
   );
@@ -19,8 +19,8 @@ test('test encrypt', () => {
   const commonPk = ECIES.publicKey(commonSk);
   const sk = ECIES.generateSecretKey();
   const pk = ECIES.publicKey(sk);
-  const message = toBN(1234);
+  const message = toBN(FIELD_SIZE.subn(10));
   const encrypted = ECIES.encrypt(message, pk, commonSk);
   const decrypted = ECIES.decrypt(encrypted, sk, commonPk);
-  expect(decrypted.toNumber()).toBe(message.toNumber());
+  expect(decrypted.toString()).toBe(message.toString());
 });
