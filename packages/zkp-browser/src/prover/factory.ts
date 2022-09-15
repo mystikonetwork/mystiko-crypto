87c0e3296d1ffd93e7093875185981b73e218ddd
Fitch Li <fitch@mystiko.network>
2022-09-15 13:28:24 +0800

    fix issues when running protocol in browser (#15)

--- packages/zkp-browser/src/prover/factory.ts
+++ packages/zkp-browser/src/prover/factory.ts
@@ -5,6 +5,15 @@ import { ZokratesBrowserProver } from './zokrates';
 export class ZokratesBrowserProverFactory implements ZKProverFactory {
   // eslint-disable-next-line class-methods-use-this
   public create(): Promise<ZokratesBrowserProver> {
-    return initialize().then((provider: ZoKratesProvider) => new ZokratesBrowserProver(provider));
+    return initialize().then(
+      (provider: ZoKratesProvider) =>
+        new ZokratesBrowserProver(
+          provider.withOptions({
+            backend: 'bellman',
+            scheme: 'g16',
+            curve: 'bn128',
+          }),
+        ),
+    );
   }
 }
