2cd6547bc1a2b4d5ad0d9fd83a30b5e9f0aa0756
Fitch Li <fitch@mystiko.network>
2023-09-24 00:23:47 +0800

    fix web worker does not work correctly in browser (#27)

--- packages/protocol/src/protocol/v2/worker.ts
+++ packages/protocol/src/protocol/v2/worker.ts
@@ -1,16 +1,31 @@
-import { toBN, toBuff } from '@mystikonetwork/utils';
-import { DecryptOutput } from '../../interface';
+import { toBN, toBuff, toHex } from '@mystikonetwork/utils';
+import { DecryptStringOutput } from '../../interface';
 import { decryptNotes as decryptNotesImpl } from './impl';
 
 export function decryptNotes(
   commitments: { commitmentHash: string; encryptedNote: string }[],
   keys: { publicKey: string; secretKey: string }[],
-): Promise<DecryptOutput[]> {
+): Promise<DecryptStringOutput[]> {
   return decryptNotesImpl(
     commitments.map(({ commitmentHash, encryptedNote }) => ({
       commitmentHash: toBN(commitmentHash),
       encryptedNote: toBuff(encryptedNote),
     })),
     keys.map(({ publicKey, secretKey }) => ({ publicKey: toBuff(publicKey), secretKey: toBuff(secretKey) })),
+  ).then((outputs) =>
+    outputs.map((output) => ({
+      commitment: {
+        encryptedNote: toHex(output.commitment.encryptedNote),
+        shieldedAddress: output.commitment.shieldedAddress,
+        commitmentHash: output.commitment.commitmentHash.toString(),
+        amount: output.commitment.amount.toString(),
+        randomP: output.commitment.randomP.toString(),
+        randomR: output.commitment.randomR.toString(),
+        randomS: output.commitment.randomS.toString(),
+        k: output.commitment.k.toString(),
+      },
+      shieldedAddress: output.shieldedAddress,
+      serialNumber: output.serialNumber.toString(),
+    })),
   );
 }
